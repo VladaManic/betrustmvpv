9bbc83adc30052f4963c80b3051fbb07d9245481
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2022-10-25 11:32:03 +0200

    fetch API from json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import GeneralStyles from './shared/styles/GeneralStyles';
 import GlobalFonts from './assets/fonts/fonts';
@@ -11,6 +11,59 @@ import MyBets from './pages/MyBets'
 import PreMatch from './pages/PreMatch/PreMatch'
 
 function App() {
+  //useEffect(() => {
+		// fetch('http://localhost:3000/sport')
+		// 	.then((res) => res.json())
+		// 	.then((result) => {
+		// 			console.log(result);
+		// 	})
+    // fetch(
+    //   'http://localhost:3000/sport',
+    // ).then((response) => {
+    //   return response.json();
+    // }).then((data) => {
+    //   const items = [];
+    //   for(const key in data){
+    //     const item = {
+    //       id: key,
+    //       ...data[key]
+    //     }
+    //     items.push(item)
+    //   }
+    //   console.log(items);
+    // })
+    
+	//}, [])
+  // const getData=()=>{
+  //   fetch('http://localhost:4000/sport'
+  //   ,{
+  //     headers : { 
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //      }
+  //   }).then(function(response){
+  //       console.log(response)
+  //       return response.json();
+  //     })
+  //     .then(function(myJson) {
+  //       console.log(myJson);
+  //     });
+  // }
+  // useEffect(()=>{
+  //   getData()
+  // },[])
+//   useEffect(() => {
+//     fetch('http://localhost:4000/sport')
+//     .then((res) => res.json())
+//     .then(function(response){
+//       console.log(response)
+//       return response.json();
+//     })
+//     .then(function(myJson) {
+//       console.log(myJson);
+//     });
+// }, []);
+
   return (
     <div className="App">
       <GeneralStyles />
--- src/pages/InPlay/index.tsx
+++ src/pages/InPlay/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { observer } from "mobx-react"
 import store from '../../store/store'
 
 const InPlay = () => {
+	const fetchData = () => {
+    fetch("http://localhost:4000/sport")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+	useEffect(() => {
+    fetchData();
+  }, []);
+
 	return (
 		<div>
-			{ store.todos.map((todo: {title: string, text: string}) => (
-				<h2>{todo.text}</h2>
+			{ store.todos.map((todo: {id: number, title: string, text: string}) => (
+				<h2 key={todo.id}>{todo.text}</h2>
 			))}
 		</div>
 	)
